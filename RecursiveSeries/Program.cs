9d36a30ec9fd945c041d7ce4846b2c7707e9ea53
aishanoor1439 <aisha.noor1439@gmail.com>
2025-09-10 21:09:48 +0500

    First commit
    completed

---
+++ RecursiveSeries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveSeries
+{
+    internal class Program
+    {
+        static void PrintSeries(int current, int n)
+        {
+            if (current > n) 
+                return;
+
+            Console.Write(current + "\t");
+            PrintSeries(current * 2, n); 
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the last number (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSeries:");
+            PrintSeries(2, n);
+        }
+    }
+}
